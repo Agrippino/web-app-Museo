847076ea68311194aaad693fc91b2a067a1408f3
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-26 17:08:08 +0200

    Aggiunti public ai db set

--- web-app-Museo/Data/MuseoContext.cs
+++ web-app-Museo/Data/MuseoContext.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using web_app_Museo.Models;
 
 namespace web_app_Museo.Data
 {
     public class MuseoContext : DbContext
     {
-        DbSet<Prodotto> Prodotti { get; set; }
-        DbSet<Acquisto> Acquisti { get; set; }
-        DbSet<Categoria> Categorie { get; set; }
-        DbSet<Rifornimento> Rifornimenti { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<Prodotto> Prodotti { get; set; }
+        public DbSet<Acquisto> Acquisti { get; set; }
+        public DbSet<Categoria> Categorie { get; set; }
+        public DbSet<Rifornimento> Rifornimenti { get; set; }
+        public protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=localhost;Database=DbMuseo; Integrated Security=True");
         }
