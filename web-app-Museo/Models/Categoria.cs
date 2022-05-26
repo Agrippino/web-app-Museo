c30c46a573cb9b573d280ec3a11bf59fd190a0c4
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-26 16:52:31 +0200

    Implementata TabellaPonte di categoria con prodotti

--- web-app-Museo/Models/Categoria.cs
+++ web-app-Museo/Models/Categoria.cs
@@ -1,8 +1,12 @@
-﻿namespace web_app_Museo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_app_Museo.Models
 {
     public class Categoria
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Il campo Nome è obbligatorio")]
         public string Nome { get; set; }
         public List<Prodotto> Prodotti { get; set; }
 
---
+++ web-app-Museo/Models/CategorieProdotti.cs
@@ -0,0 +1,8 @@
+﻿namespace web_app_Museo.Models
+{
+    public class CategorieProdotti
+    {
+        public Prodotto Prodotti { get; set; }
+        public List<Categoria>? Categorie { get; set; }
+    }
+}
