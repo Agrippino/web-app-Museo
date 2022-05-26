260f847ac54b1a5e4d1cff595378ae87bc7c1952
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-26 16:18:23 +0200

    Creato modello Rifornimento- Jolly

---
+++ web-app-Museo/Models/Rifornimento.cs
@@ -0,0 +1,19 @@
+﻿namespace web_app_Museo.Models
+{
+    public class Rifornimento
+    {
+        public int Id { get; set; }
+        public int QuantitaDaAggiungere { get; set; }
+
+
+        public int? ProdottoId { get; set; }
+        public Prodotto? prodotto { get; set; }
+
+        public Rifornimento()
+        {
+
+        }
+
+
+    }
+}
