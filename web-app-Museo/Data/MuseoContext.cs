c4cc8d2dc46b0eac66d3913cf184dee09c005f46
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-26 16:02:27 +0200

    Aggiunte tutte le tabelle al database

--- web-app-Museo/Data/MuseoContext.cs
+++ web-app-Museo/Data/MuseoContext.cs
@@ -5,6 +5,9 @@ namespace web_app_Museo.Data
     public class MuseoContext : DbContext
     {
         DbSet<Prodotto> Prodotti { get; set; }
+        DbSet<Acquisto> Acquisti { get; set; }
+        DbSet<Categoria> Categorie { get; set; }
+        DbSet<Rifornimento> Rifornimenti { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=localhost;Database=DbMuseo; Integrated Security=True");
