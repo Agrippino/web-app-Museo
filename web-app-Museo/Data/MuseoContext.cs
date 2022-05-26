6cf4928f132f018aa5b9f06f4402c9023f995f9f
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-26 15:58:56 +0200

    Aggiunto Db Museo Context

---
+++ web-app-Museo/Data/MuseoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace web_app_Museo.Data
+{
+    public class MuseoContext : DbContext
+    {
+        DbSet<Prodotto> Prodotti { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=DbMuseo; Integrated Security=True");
+        }
+    }
+}
