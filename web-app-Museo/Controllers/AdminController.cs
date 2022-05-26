46107572588e1052ccc362ab1e4d0a95fade6eb9
Muoso <russo_luca@outlook.com>
2022-05-26 15:59:16 +0200

    Aggiunto Metodo Controller Index in AdminController

--- web-app-Museo/Controllers/AdminController.cs
+++ web-app-Museo/Controllers/AdminController.cs
@@ -8,5 +8,42 @@ namespace web_app_Museo.Controllers
         {
             return View();
         }
+
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Prodotto> listaProdotti = new List<Prodotto>();
+            using (MuseoContext db = new MuseoContext())
+            {
+                listaProdotti = db.Prodotti.ToList();
+            }
+
+          
+            return View("Index", listaProdotti);
+        }
+
+        /*
+        public IActionResult Modifica(int? id)
+        {
+            //Se il database è vuoto o l'id nullo ritorno notfound
+            if (id == null || db.Prodotti == null)
+            {
+                return NotFound();
+            }
+            //Se non c'è l'id ritorno notfound
+            var travelPackageModel =  db.Prodotti.FindAsync(id);
+            if (travelPackageModel == null)
+            {
+                return NotFound();
+            }
+            return View(travelPackageModel);
+        }
+
+        */
+
+
+
     }
 }
