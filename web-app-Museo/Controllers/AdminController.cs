142e212b49dda231801f0b42cefdfa8b53f968b7
Muoso <russo_luca@outlook.com>
2022-05-26 15:23:18 +0200

    Commit di prova

---
+++ web-app-Museo/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_app_Museo.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
