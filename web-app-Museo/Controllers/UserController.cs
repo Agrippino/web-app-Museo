91a8a72698624563fef548c5b2e41332146f0c44
Muoso <russo_luca@outlook.com>
2022-05-30 10:08:07 +0200

    Aggiunto UserController e view index

---
+++ web-app-Museo/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_app_Museo.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
