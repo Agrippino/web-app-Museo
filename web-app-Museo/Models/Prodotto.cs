d48f80ccc31606734b095e6280ab25658aba54f8
Muoso <russo_luca@outlook.com>
2022-05-30 17:54:28 +0200

    Modifiche all'API Prodotti per fare le Join in Entity Framework

--- web-app-Museo/Models/Prodotto.cs
+++ web-app-Museo/Models/Prodotto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace web_app_Museo.Models
 {
@@ -31,6 +32,7 @@ namespace web_app_Museo.Models
         }
 
         public int? CategoriaId { get; set; }
+        
         public Categoria? Categorie { get; set; }
         public List<Acquisto>? Acquisti { get; set; }
         public List<Rifornimento>? Rifornimenti { get; set; }
