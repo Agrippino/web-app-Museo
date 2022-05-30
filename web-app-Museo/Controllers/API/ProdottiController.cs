2c48d9ee7ec2ec003fbda75af4e3af1b5e075d4d
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-30 14:25:09 +0200

    Aggiunto dettagli nel controller api

--- web-app-Museo/Controllers/API/ProdottiController.cs
+++ web-app-Museo/Controllers/API/ProdottiController.cs
@@ -20,5 +20,24 @@ namespace web_app_Museo.Controllers.API
             }
                 return Ok(listaProdotti);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetId(int? id)
+        {
+            using (MuseoContext db = new MuseoContext())
+            {
+                if (id != null)
+                {
+
+                    Prodotto info = db.Prodotti.Where(info => info.Id == id).FirstOrDefault<Prodotto>();
+                    return Ok(info);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+
     }
 }
