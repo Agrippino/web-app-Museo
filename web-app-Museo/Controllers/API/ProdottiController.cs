4d740e1cc8a212d190d33ca1ea37fe14c3864c46
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-30 11:26:09 +0200

    Aggiunto metodo get in api, che converte i nostri prodotti in oggetti JSON

---
+++ web-app-Museo/Controllers/API/ProdottiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web_app_Museo.Data;
+using web_app_Museo.Models;
+
+namespace web_app_Museo.Controllers.API
+{
+    [Route("api/[controller/Action]")]
+    [ApiController]
+    public class ProdottiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Prodotto> listaProdotti = new List<Prodotto>();
+            using (MuseoContext db = new MuseoContext())
+            {
+                listaProdotti = db.Prodotti.ToList();
+
+            }
+                return Ok(listaProdotti);
+        }
+    }
+}
