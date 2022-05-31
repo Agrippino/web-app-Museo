b478f35620e4193330dd6b9defdadf607a4edc9d
jollyfellow7 <simonecompagnone7@gmail.com>
2022-05-31 17:33:52 +0200

    Aggiunte nuove migration

---
+++ SQLDbRifornimento.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW quantitaDisponibile
+AS
+SELECT
+    r.DataRifornimento,
+    p.Id,
+    p.Nome,
+    SUM(QuantitaDaAggiungere + p.QuantitaDisponibile) AS QuantitaTotale
+FROM
+    Rifornimenti AS r
+INNER JOIN Prodotti AS p
+    ON p.Id = r.ProdottoId
+GROUP BY r.DataRifornimento,
+         p.Id,
+         p.Nome;
