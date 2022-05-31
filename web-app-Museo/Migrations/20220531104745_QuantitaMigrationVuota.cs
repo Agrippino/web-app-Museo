eb000039c9c4e56f2b00f49aa255cd1b0b7e26fb
Muoso <russo_luca@outlook.com>
2022-05-31 18:42:05 +0200

    Push di allineamento

--- web-app-Museo/Migrations/20220531104745_QuantitaMigrationVuota.cs
+++ web-app-Museo/Migrations/20220531104745_QuantitaMigrationVuota.cs
@@ -8,27 +8,11 @@ namespace web_app_Museo.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"CREATE VIEW QuantitaDisponibili
-                                        AS
-                                        SELECT
-                                            r.DataRifornimento,
-                                            p.Id,
-                                            p.Nome,
-                                            SUM(QuantitaDaAggiungere + p.QuantitaDisponibile) AS QuantitaTotale
-                                        FROM
-                                            Rifornimenti AS r
-                                        INNER JOIN Prodotti AS p
-                                            ON p.Id = r.ProdottoId
-                                        GROUP BY 
-                                         r.DataRifornimento,
-                                        p.Id,
-                                      p.Nome;");
+           
         }
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"
-            drop view QuantitaDisponibili;
-            ");
+          
         }
     }
 }
