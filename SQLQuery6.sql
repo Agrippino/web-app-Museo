47d05cdba0e7b5b949e03c77476981e04af93104
Muoso <russo_luca@outlook.com>
2022-06-02 01:43:47 +0200

    aggiornata view rifornimento

---
+++ SQLQuery6.sql
@@ -0,0 +1,17 @@
+﻿SELECT
+    p.Id,
+    p.Nome,
+    SUM(d.QuantitaTotale - r.QuantitaTotale) AS QuantitaTotale
+FROM
+    Prodotti as p
+    INNER JOIN
+    QuantitaAggiunte AS d
+    ON d.id = p.id
+    LEFT JOIN
+        QuantitaAcquistate AS r
+    ON d.Id = r.Id
+    GROUP BY
+        p.Id,
+        p.Nome
+    ORDER BY QuantitaTotale ASC
+        ;
