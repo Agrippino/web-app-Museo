1385d8d4ac7ed242e3f95cfe992e4e71f74939d8
Muoso <russo_luca@outlook.com>
2022-06-03 12:39:23 +0200

    QuantitaDisponibile nuova migration

--- web-app-Museo/Migrations/20220601225303_QuantitaDisponibileMigration2.cs
+++ web-app-Museo/Migrations/20220601225303_QuantitaDisponibileMigration2.cs
@@ -8,38 +8,13 @@ namespace web_app_Museo.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"Create VIEW QuantitaDisponibili
-                                AS
-                                SELECT
-                                    p.Id,
-                                    p.Nome,
-                                    p.Immagine,
-                                    c.Nome AS Categoria,
-                                    SUM(d.QuantitaTotale - r.QuantitaTotale) AS QuantitaTotale
-                                FROM
-                                    QuantitaAcquistate AS r
-                                    INNER JOIN
-                                        QuantitaAggiunte AS d
-                                    ON r.Id = d.Id
-                                    INNER JOIN
-                                        Prodotti AS p
-                                    ON d.Id = p.Id
-                                    INNER JOIN
-                                        Categorie AS c
-                                    ON p.CategoriaId = c.Id
-                                    GROUP BY
-                                        p.Id,p.Nome,p.Immagine,c.Nome
-                                    ORDER BY
-                                        QuantitaTotale 
-                                    OFFSET 0 ROWS;");
+            
             
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"
-            drop view QuantitaDisponibili;
-            ");
+            
         }
     }
 }
