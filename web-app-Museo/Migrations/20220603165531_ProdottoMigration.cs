31630b3bbed8dd1a808c7351804d0ea16d0b9a64
jollyfellow7 <simonecompagnone7@gmail.com>
2022-06-06 10:17:34 +0200

    corretto rifornimento

--- web-app-Museo/Migrations/20220603165531_ProdottoMigration.cs
+++ web-app-Museo/Migrations/20220603165531_ProdottoMigration.cs
@@ -8,26 +8,12 @@ namespace web_app_Museo.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<int>(
-                name: "like",
-                table: "Prodotti",
-                type: "int",
-                nullable: true,
-                oldClrType: typeof(int),
-                oldType: "int");
+
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<int>(
-                name: "like",
-                table: "Prodotti",
-                type: "int",
-                nullable: false,
-                defaultValue: 0,
-                oldClrType: typeof(int),
-                oldType: "int",
-                oldNullable: true);
+
         }
     }
 }
